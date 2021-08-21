ef85db85b64703904f74fa9f5282ef13e9cfb251
Berrones Edwin <Berrones.Edwin@hotmail.com>
2021-08-21 15:54:11 -0500

    Configure chakra ui

---
+++ src/theme/theme.ts
@@ -0,0 +1,11 @@
+import { extendTheme } from '@chakra-ui/react'
+
+export const theme = extendTheme({
+  colors: {
+    bgColor: '#101011',
+    bgColorSeconday: '#3f353c',
+    bgColorTerciary: '#d6c1b5',
+    primary: '#b13770',
+    secondary: '#8d797b'
+  }
+})
