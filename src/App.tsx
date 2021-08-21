e71be94097da443928a59c98a078e5d233842e68
Berrones Edwin <Berrones.Edwin@hotmail.com>
2021-08-21 16:34:19 -0500

    Create navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
-import React, { useState } from 'react'
-import { Button } from '@chakra-ui/react'
+import React from 'react'
+import { Button, Stack } from '@chakra-ui/react'
 
 function App() {
-  return <Button bgColor="primary">Click me</Button>
+  return (
+    <Stack bgColor="bgColor" minW="100vw" minH="100vh">
+      <Button bgColor="primary">Click me</Button>
+    </Stack>
+  )
 }
 
 export default App
