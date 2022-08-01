88fefebb1c37bc9fdb392c6950332e0b2f344217
Berrones Edwin <Berrones.Edwin@hotmail.com>
2022-08-01 16:00:03 -0500

    chore: change version react 18 -> 17

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,26 @@
 import React from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom'
 import App from './App'
 import './styles.css'
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
 import { theme } from './theme/theme'
 
 
-const root = createRoot(document.getElementById('root') as HTMLElement);
-root.render(
-  <React.StrictMode>
-    <ChakraProvider theme={theme} >
-      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      <App />
-    </ChakraProvider>
-  </React.StrictMode>
-)
-// ReactDOM.render(
+// const root = createRoot(document.getElementById('root') as HTMLElement);
+// root.render(
 //   <React.StrictMode>
-//     <ChakraProvider theme={theme}>
+//     <ChakraProvider theme={theme} >
 //       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
 //       <App />
 //     </ChakraProvider>
-//   </React.StrictMode>,
-//   document.getElementById('root')
+//   </React.StrictMode>
 // )
+ReactDOM.render(
+  <React.StrictMode>
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
